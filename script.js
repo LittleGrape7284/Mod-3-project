b471f00fb06d91f070100181fab4e97fb77e388e
akilfoil26 <akilfoil26@students.stab.org>
2025-12-18 10:43:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ var sodium = getColumn(url, 10)
 
 //function that takes the parameters selectedType, selectedCal, proteinChoice
 function getResults(selectedType, selectedCal, proteinChoice){
-    //making a list of 
+    //making a list of foods that match these parameters 
     var matchingFoods=[];
+    //cycling though the list of foodNames one by one
     for (var i=0; i < foodName.length; i++){
-       
-        if (type[i]==selectedType && parseFloat(protein[i])> parseInt(proteinChoice) && calories[i]< selectedCal){    
+       //if we find a food that = the type of food we selected in out drop down menu & the protein number is above the one we selcted & the calories are less than the number we slided
+        if (type[i]==selectedType && parseFloat(protein[i])> parseInt(proteinChoice) && calories[i]< selectedCal){ 
+        //then we push that food to out list   
         matchingFoods.push(foodName[i])
        
     }
@@ -25,7 +27,7 @@ console.log(matchingFoods)
 for (var i=0; i < matchingFoods.length; i++){
     //create a paragraph element for each matching name
     var food = document.createElement("p");
-    //set the inner HTML of that new element to be the matching dog name
+    //set the inner HTML of that new element to be the matching food name
     food.innerHTML=matchingFoods[i];
     //apend the name to the outputBox
     document.getElementById("outputBox").appendChild(food);
@@ -35,10 +37,11 @@ for (var i=0; i < matchingFoods.length; i++){
 
 
 
-function showCal(){ //defined function called showTemp
-    var size= document.getElementById("selectedCal").value; // Declares a variable 'size' and assigns it the value of an HTML element with id "size".
-    document.getElementById("sliderVal").innerHTML= size +" Calories or less" // Updates the inner HTML of an element with id "feedback" to display the 'size' value followed by " °F".
+function showCal(){ //defined function called showCal
+    var size= document.getElementById("selectedCal").value; // Declares a variable 'size' and assigns it the value of an HTML element with id "selected Cal".
+    document.getElementById("sliderVal").innerHTML= size +" Calories or less" // Updates the inner HTML of an element with id "sliderVal" to display the 'size' value followed by " Calories or Less".
 }
 
 showCal();
+
 
