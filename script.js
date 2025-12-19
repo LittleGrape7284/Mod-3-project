20cd87ab2e9cfb078c5142f3c443bf28a305b4ac
LittleGrape7284 <jdruzgal29@students.stab.org>
2025-12-19 09:36:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+// Dataset Source: "Fast Food Nutrition" CSV from Blair McAvoy GitHub
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Science/Fast%20Food%20Nutrition.csv"
 var foodName = getColumn(url, 1)
 var type = getColumn(url, 2)
@@ -9,33 +12,50 @@ var sodium = getColumn(url, 10)
 
 //function that takes the parameters selectedType, selectedCal, proteinChoice
 function getResults(selectedType, selectedCal, proteinChoice){
-    //making a list of foods that match these parameters 
+    //making a list of matching foods
     var matchingFoods=[];
-    //cycling though the list of foodNames one by one
     for (var i=0; i < foodName.length; i++){
-       //if we find a food that = the type of food we selected in out drop down menu & the protein number is above the one we selcted & the calories are less than the number we slided
-        if (type[i]==selectedType && parseFloat(protein[i])> parseInt(proteinChoice) && calories[i]< selectedCal){ 
-        //then we push that food to out list   
-        matchingFoods.push(foodName[i])
-       
+         //if we find a food that = the type of food we selected in out drop down menu & the protein number is above the one we selcted & the calories are less than the number we slided
+        if (type[i]==selectedType && parseFloat(protein[i])> parseInt(proteinChoice) && parseFloat(calories[i]) < parseInt(selectedCal)){   
+             //then we push that food to out list   
+            matchingFoods.push(foodName[i])
+        }
     }
-}
-console.log(matchingFoods)
-//this will clear the output Box before appending new items
- document.getElementById("outputBox").innerHTML=""
-//loop through my list of matches
-for (var i=0; i < matchingFoods.length; i++){
+
+  //this will clear the output Box before appending new items
+    document.getElementById("outputBox").innerHTML = "";
+
+   //loop through my list of matches
     //create a paragraph element for each matching name
-    var food = document.createElement("p");
-    //set the inner HTML of that new element to be the matching food name
-    food.innerHTML=matchingFoods[i];
-    //apend the name to the outputBox
-    document.getElementById("outputBox").appendChild(food);
+    var count = document.createElement("p");
+     //set the inner HTML of that new element to be the matching food name
+    count.innerHTML = "Number of matching foods found: " + matchingFoods.length;
+    //append the item to the output box
+    document.getElementById("outputBox").appendChild(count);
+
+    // If no matches, show message and stop
+    if (matchingFoods.length === 0) {
+          //create a new paragraph element
+        var none = document.createElement("p");
+        //Display a message to the user when the operation has no results.
+        none.innerHTML = "No matching foods found";
+        //Appends the message to the output box
+        document.getElementById("outputBox").appendChild(none);
+        return;
+    }
+
+    //loop through list of matches
+    for(var i = 0; i < matchingFoods.length; i++){
+        console.log(i);
+        //Creates a new paragraoh element
+        var food = document.createElement("p");
+        //set the inner HTML of that new element to be the matching food name
+        food.innerHTML = matchingFoods[i];
+        //Appends the item to the output box
+        document.getElementById("outputBox").appendChild(food);
+    }
 
 }
-}
-
-
 
 function showCal(){ //defined function called showCal
     var size= document.getElementById("selectedCal").value; // Declares a variable 'size' and assigns it the value of an HTML element with id "selected Cal".
